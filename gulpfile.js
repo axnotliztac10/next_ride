487a1a6a7a1407e5c8b3be2287fba1d141bab250
axnotliztac10 <axnotliztac@gmail.com>
2015-03-27 16:32:30 -0600

    Added fixes

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,21 @@ gulp.paths = {
 
 require('require-dir')('./gulp');
 
+var s3 = require('gulp-s3-upload')({
+        accessKeyId:        "AKIAIALST2BBYYVUMC6A",
+        secretAccessKey:    "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"
+    });
+
+gulp.task('publish', function() {
+ 
+	gulp.src('dist/**')
+    .pipe(s3({
+        Bucket: 'next-ride',
+        ACL:    'public-read'
+    }));
+ 
+});
+
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
